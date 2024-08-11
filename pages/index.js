123b224c3f04a00eeb625361c927cfe8dd1141d0
petergya <petergyang82@gmail.com>
2024-08-11 22:09:55 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -15,15 +15,15 @@ import { fadeIn } from "../variants"
 
 const Home = () => {
   return (
-  <div className="bg-primary/60 h-full ">
+  <div className="bg-primary/60 h-screen ">
 <div className="w-full h-full bg-gradient-to-r from-primary/10 via-blacl/30 to-black/10 ">
-  <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto  ">
+  <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full  container mx-auto  ">
     <motion.h1 
     variants={fadeIn("down", 0.2)} 
     initial="hidden"
      animate="show"
      exit="hidden"
-    className="h1"
+    className="h1 mt-20"
     >
       Transforming Ideas <br /> Into <span className="text-accent">Digital Reality</span>
     </motion.h1>

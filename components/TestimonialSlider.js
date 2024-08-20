d02ce0af07ac75c4d414f4ffecb9755e501dca91
petergya <petergyang82@gmail.com>
2024-08-20 17:05:34 +0100

    add navigaton to testimonials

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -3,8 +3,9 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/free-mode";
+import "swiper/css/navigation";
 import "swiper/css/pagination";
-import { Pagination } from "swiper";
+import { Navigation, Pagination } from "swiper";
 import { FaQuoteLeft } from 'react-icons/fa';
 import Image from "next/image";
 
@@ -15,21 +16,21 @@ const testimonialData = [
     name: 'Anne Smith',
     position: 'Customer',
     message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam!',
   },
   {
     image: '/t-avt-2.png',
     name: 'Jane Doe',
     position: 'Customer',
     message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam !',
   },
   {
     image: '/t-avt-3.png',
     name: 'John Doe',
     position: 'Customer',
     message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam !',
   }
 ];
 
@@ -37,22 +38,24 @@ const TestimonialSlider = () => {
   return (
     <div className="relative">
       <Swiper
+      navigation={true}
         spaceBetween={10}
         pagination={{
           clickable: true,
           el: '.custom-pagination',
         }}
-        modules={[Pagination]}
-        className="h-[300px] sm:h-[300px] lg:h-[350px]"
+        modules={[Navigation, Pagination]}
+        className="h-[250px] sm:h-[150px] "
       >
         {testimonialData.map((slide, slideIndex) => (
           <SwiperSlide key={slideIndex}>
-            <div className="flex flex-col lg:flex-row items-center gap-x-8 h-full px-16">
+            <div className="flex flex-col md:flex-row items-center gap-x-8 h-full px-16">
               {/* Avatar, Name, Position */}
-              <div className='w-full max-w-[300px] lg:w-auto'>
-                <div className="flex flex-col items-center lg:items-start">
+              <div className='w-full max-w-[300px]  flex flex-col xl:justify-center items-center
+              relative mx-auto xl:mx-0'>
+                <div className="flex flex-col justify-center text-center">
                   {/* Avatar */}
-                  <div className="mb-4">
+                  <div className="mb-2 mx-auto">
                     <Image 
                       src={slide.image} 
                       alt={slide.name} 
@@ -62,14 +65,14 @@ const TestimonialSlider = () => {
                     />
                   </div>
                   {/* Name */}
-                  <div className="text-xl font-semibold">{slide.name}</div>
+                  <div className="text-lg font-semibold">{slide.name}</div>
                   {/* Position */}
-                  <div className="text-gray-500">{slide.position}</div>
-                </div>
+                  <div className="text-[12px] uppercase font-extralight tracking-widest">{slide.position}</div>
+                </div> 
               </div>
               
               {/* Quote Icon & Message */}
-              <div className=" flex-1 flex flex-col justify-center w-full
+              <div className="  flex-1 flex flex-col justify-center 
               before:w-[1px] xl:before:bg-white/20 xl:before:absolute 
               xl:before:left-0 xl:before:h-[200px] relative xl:pl-20">
                 {/* Quote Icon */}
